96a8fe7cb24ceb4577f8449b4319cccd371b55a2
Kholy191 <a.adel27111@gmail.com>
2025-02-20 06:27:22 +0200

    Q1[Element]

---
+++ Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1(Element) Get first Product out of Stock 
+
+            //var Result = ListGenerator.ProductList.Where(P => P.UnitsInStock == 0).FirstOrDefault();
+
+            //Console.WriteLine(Result);
+
+            #endregion
+
+        }
+    }
+}
